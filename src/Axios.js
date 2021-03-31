9204ed54ae17f134f6c36ea0985b0b9328542c2a
alfazkhan <khanalfaaz14@gmail.com>
2021-03-31 23:16:17 +0530

    Base api changed

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { decryption } from 'helpers/encryption';
 
 const Axios = axios.create({
-  baseURL: `http://api.shyamchemicals.co.in/`,
+  baseURL: `https://api.shyamchemicals.co.in/`,
   headers:{
     'Content-Type': 'application/json',
    
