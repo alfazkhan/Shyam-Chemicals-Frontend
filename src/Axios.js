7bbf74949dcf275097da038b1a950202fda0bc22
alfazkhan <khanalfaaz14@gmail.com>
2021-04-01 10:39:45 +0530

    api endpoint fix

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { decryption } from 'helpers/encryption';
 
 const Axios = axios.create({
-  baseURL: `https://api.shyamchemicals.co.in/`,
+  baseURL: `https://api.shyamchemical.co.in/`,
   headers:{
     'Content-Type': 'application/json',
    
